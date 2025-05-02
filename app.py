148b1728e4030cfa4a46eef7c4a9dded5694fe82
purvapr13 <purvapr13@gmail.com>
2025-05-02 17:02:01 +0530

    added logger

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
 from fastapi import FastAPI
+import uvicorn
+
+from app.utils.logger import configure_logging
+import logging
+
+configure_logging()
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_ques():
+    logger.info('Getting question from the user:')
     return {"Please wait..."}
 
 
 @app.get("/predict")
 def response_gen(ques: str):
     try:
+        logger.info('Generating answer:')
         print("write a function")
     except Exception as e:
         return {"sorry, couldn't process the request."}
+
+
+# Main function to run the app
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000)
+
 
---
+++ app/utils/logger.py
@@ -0,0 +1,22 @@
+# logger.py
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+def configure_logging():
+    # Configure root logger once
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    if not logger.handlers:
+        formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(name)s - %(filename)s:%(lineno)d - %(message)s"
+        )
+
+        file_handler = RotatingFileHandler("app.log", maxBytes=5 * 1024 * 1024, backupCount=3)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
